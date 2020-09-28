8da00fa35c338e8d9c54ef67faf05973ab4c9ef3
Alan Truong <alan.truong2@gmail.com>
2020-09-28 22:44:48 +1000

    init express and simple REST endpt

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-console.log('hello me over there');
-
 import express from 'express';
+
+const main = async () => {
+  const app = express();
+
+  app.get('/', (req, res) => {
+    res.send('Hi');
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
