2d0106292ba1f8a95b7cd0095a4658c5f2f417b4
Alan Truong <alan.truong2@gmail.com>
2020-09-28 23:45:44 +1000

    fix: dev script

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import express from 'express';
 const main = async () => {
   const app = express();
 
-  app.get('/', (req, res) => {
+  app.listen(4000, () => {
+    console.log('server astarted on localhost:4000');
+  });
+
+  app.get('/', (_req, res) => {
     res.send('Hi');
   });
 };
