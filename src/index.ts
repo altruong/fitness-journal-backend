5f6f48b6bde86abc710c8c26f2a54d9b48ded8b1
Alan Truong <alan.truong2@gmail.com>
2020-09-29 18:07:39 +1000

    install typeorm and pg /setup ormconfig

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
-
+import { createConnection } from 'typeorm';
 const main = async () => {
+  // createConnection method will automatically read connection options
+  // from your ormconfig file or environment variables
+  const conn = await createConnection();
+
   const app = express();
 
   app.listen(4000, () => {
