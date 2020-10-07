16d56cd7fd228c78e0eb6b9f7ebe340f0726794b
Alan Truong <alan.truong2@gmail.com>
2020-10-07 21:03:43 +1100

    added apollo server

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,3 +1,4 @@
+import { Field, ObjectType } from 'type-graphql';
 import {
   BaseEntity,
   Column,
@@ -6,20 +7,25 @@ import {
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
+@ObjectType() // Type-graphql decorator
 @Entity() // Type-orm decorator
 export class Post extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id!: number;
 
+  @Field()
   @Column()
   title!: string;
 
+  @Field()
   @Column()
   text!: string;
 
+  @Field()
   @CreateDateColumn()
   createdAt: Date;
 
+  @Field()
   @CreateDateColumn()
   updatedAt: Date;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata'; // required to make the type reflection work
 import express from 'express';
 import { createConnection } from 'typeorm';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from './resolvers/post';
 require('dotenv').config();
 
 const main = async () => {
@@ -9,6 +12,11 @@ const main = async () => {
   const conn = await createConnection();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({ resolvers: [PostResolver], validate: false }),
+  });
+  apolloServer.applyMiddleware({ app }); // Creates a graphql endpoint on express
 
   app.listen(4000, () => {
     console.log('server astarted on localhost:4000');
