7c7243d82aba986e60572a8ffda5172ccf80e80e
Alan Truong <alan.truong2@gmail.com>
2020-10-10 02:18:26 +1100

    add cors origin to express

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { createConnection } from 'typeorm';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { PostResolver } from './resolvers/post';
+import cors from 'cors';
 require('dotenv').config();
 
 const main = async () => {
@@ -12,11 +13,15 @@ const main = async () => {
   const conn = await createConnection();
 
   const app = express();
+  app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({ resolvers: [PostResolver], validate: false }),
   });
-  apolloServer.applyMiddleware({ app }); // Creates a graphql endpoint on express
+
+  // Creates a graphql endpoint on express
+  // CORS set to false because we already have a cors middleware on app
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
     console.log('server astarted on localhost:4000');
