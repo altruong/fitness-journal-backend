39e1e3e8bd6d5d9f3b5589523a9c6c1e81821d9c
Alan Truong <alan.truong2@gmail.com>
2020-10-17 01:53:46 +1100

    create user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,33 @@
+import { Field, ObjectType } from 'type-graphql';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType() // Type-graphql decorator
+@Entity() // Type-orm decorator
+export class User extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Field()
+  @Column()
+  username!: string;
+
+  @Field()
+  @Column()
+  email!: string;
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
