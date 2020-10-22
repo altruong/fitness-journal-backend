d224d4b32f5705ee04e56f6a8f77070ac87b506b
Alan Truong <alan.truong2@gmail.com>
2020-10-22 17:53:59 +1100

    add redis to client

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import 'reflect-metadata'; // required to make the type reflection work
-import express from 'express';
-import { createConnection } from 'typeorm';
 import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { PostResolver } from './resolvers/post';
 import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import 'reflect-metadata'; // required to make the type reflection work
+import { buildSchema } from 'type-graphql';
+import { createConnection } from 'typeorm';
+import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
-require('dotenv').config();
+import redis from 'redis';
+import connectRedis from 'connect-redis';
+import session from 'express-session';
+import { COOKIE_NAME, __prod__ } from './utils/constants';
 
 const main = async () => {
   // createConnection method will automatically read connection options
@@ -18,6 +22,28 @@ const main = async () => {
     cors({
       origin: process.env.CORS_ORIGIN,
       credentials: true,
+    })
+  );
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: COOKIE_NAME, //name of cookie
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true, //Disables resetting TTL when using touch. Touch signals redis that user active but modified data
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true, // you cannot access the cookie in frontend
+        sameSite: 'lax', // protecting csrf
+        secure: __prod__, // cookie only works in https
+      }, // telling express session that we're using redis
+      saveUninitialized: false,
+      secret: String(process.env.REDIS_SECRET),
+      resave: false,
     })
   );
 
