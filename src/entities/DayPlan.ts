1c87d0e4b040bd534db712d08087b1e0696fed6d
Alan Truong <alan.truong2@gmail.com>
2020-11-24 01:11:36 +1100

    refactor day plan entities

--- src/entities/DayPlan.ts
+++ src/entities/DayPlan.ts
@@ -1,21 +1,34 @@
 import { Field, Int } from 'type-graphql';
 import {
+  Column,
   CreateDateColumn,
   Entity,
+  JoinTable,
+  ManyToMany,
   ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { Exercise } from './Exercise';
 import { Program } from './Program';
 
 @Entity()
-export class Plan {
+export class DayPlan {
   @Field(() => Int)
   @PrimaryGeneratedColumn()
   id!: number;
 
   @ManyToOne(() => Program, (program) => (program as any).plan)
   program: Program;
+
+  @ManyToMany(() => Exercise)
+  @JoinTable({ name: 'day_plan_exercise' })
+  exericse: Exercise[];
+
+  // Weekday as an integer (0-6) Sunday is 0
+  @Field(() => Int)
+  @Column()
+  day: number;
 
   @Field()
   @CreateDateColumn()
